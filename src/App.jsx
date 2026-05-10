dd5d21fc71a98b9791c9f0169fa6442e11e08ec7
Franklin Klingstar <riscecorp@gmail.com>
2026-05-10 20:24:08 +0530

    changes in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
 import Login from "./pages/Login";
+
 import Register from "./pages/Register";
+
 import Dashboard from "./pages/Dashboard";
-import LanguageDropdown from "./components/LanguageDropdown";
 
 import VineyardDetails from "./pages/VineyardDetails";
+
+import LanguageDropdown from "./components/LanguageDropdown";
+
 function App() {
+
   return (
-    <BrowserRouter>
+
+    <>
+
       <LanguageDropdown />
 
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-         <Route
+
+        <Route
+          path="/"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+        <Route
           path="/vineyard"
           element={<VineyardDetails />}
         />
 
       </Routes>
-    </BrowserRouter>
+
+    </>
   );
 }
 
