c525f9f1ffa3a0699ac941be2abe2b952f3fb212
Franklin Klingstar <riscecorp@gmail.com>
2026-05-10 20:20:19 +0530

    changes in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,43 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import { LanguageProvider } from "./context/LanguageContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+import ReactDOM from "react-dom/client";
+
+import {
+  BrowserRouter
+} from "react-router-dom";
+
+import App from "./App";
+
+import {
+  AuthProvider
+} from "./context/AuthContext";
+
+import {
+  LanguageProvider
+} from "./context/LanguageContext";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+
   <React.StrictMode>
-    <LanguageProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </LanguageProvider>
+
+    <BrowserRouter
+      basename="/satellitecropmonitoring"
+    >
+
+      <LanguageProvider>
+
+        <AuthProvider>
+
+          <App />
+
+        </AuthProvider>
+
+      </LanguageProvider>
+
+    </BrowserRouter>
+
   </React.StrictMode>
 );
