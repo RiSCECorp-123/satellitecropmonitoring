9ae771271b08300a88ea78841d4f6fffc954b56f
Franklin Klingstar <riscecorp@gmail.com>
2026-05-12 20:34:02 +0530

    commit for the centain changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,8 +28,8 @@ const Login = () => {
 
   return (
     <div className="risce-login-container">
-        <h1 style ={{textAlign:"center"}}>RISCE - <h3> Satellite Crop Monitoring</h3></h1>
-        <p style ={{textAlign:"center"}}>An end-to-end solution for farmers</p>
+        <h1 style ={{textAlign:"center", color: "#fffffff"}}>RISCE-Satellite Crop Monitoring</h1>
+        <p style ={{textAlign:"center", color: "#fffffff"}}>An end-to-end solution for farmers</p>
       <div className="risce-login-box">
         <h1>{t.login}</h1>
 
